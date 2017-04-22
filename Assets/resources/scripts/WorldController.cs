04ef0f7f29b63aaf93de6f899763adcee2b2df61
Jake Backer <jbacker42@gmail.com>
2017-04-22 11:54:22 -0400

    Add moving the camera and a PetriDish

--- Assets/resources/scripts/WorldController.cs
+++ Assets/resources/scripts/WorldController.cs
@@ -17,6 +17,33 @@ public class WorldController : MonoBehaviour {
 		if (cam.orthographicSize + axis > 0 && cam.orthographicSize + axis <= 30) {
 			cam.orthographicSize += axis;
 		}
+
+		Vector3 locationModifier = new Vector3(0, 0, 0);
+
+		if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			locationModifier = new Vector3(0, 2, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			locationModifier = new Vector3(0, -2, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			locationModifier = new Vector3(-2, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			locationModifier = new Vector3(2, 0, 0);
+		}
+
+		if (!((cam.gameObject.transform.position + locationModifier).x > 10 ||
+		    (cam.gameObject.transform.position + locationModifier).x < -10 ||
+		    (cam.gameObject.transform.position + locationModifier).y > 10 ||
+			(cam.gameObject.transform.position + locationModifier).y < -10))
+		{
+			cam.gameObject.transform.position += locationModifier;
+		}
 	}
 
 	public static void SpawnBacteria(BacteriumType type) {
